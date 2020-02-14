8202d4056ff1de0a14516d601c7f43b6e756fdb7
Volodymyr Kheroim <volodymyr_kh@hotmail.com>
2020-02-14 15:03:58 +0200

    HW-11 color_switch

---
+++ JS/index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548"
+];
+
+let isActive = false;
+let backGroundColorId = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const refs = {
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector("body")
+};
+
+refs.startBtn.addEventListener("click", () => {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  backGroundColorId = setInterval(setBackGroundColor, 1000);
+});
+
+refs.stopBtn.addEventListener("click", () => {
+  clearInterval(backGroundColorId);
+  isActive = false;
+});
+
+function setBackGroundColor() {
+  let arrIndex = randomIntegerFromInterval(0, colors.length - 1);
+  refs.body.style.backgroundColor = colors[arrIndex];
+}
